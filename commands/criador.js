90b2a2accc455d39bbc0da728dbdd46a638afca6
Pedrindogithub12121 <157772318+Pedrindogithub12121@users.noreply.github.com>
2024-04-30 16:01:25 -0300

    adicionando mais comandos

---
+++ commands/criador.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "criador",
+    description: "Quem me criou?",
+    options: [],
+    run: async (client, interaction) => {
+    
+    interaction.reply("Fui criado pelo Pedro Henrique em node.js. Caso tenha interesse, o bot está no github para você poder colocar no SEU bot! link: https://github.com/CaquinhoDev/discord-slash-bot")
+      
+    },
+    };
+    
---
+++ commands/menu.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+
+module.exports = {
+    name: "menu",
+    description: "Mostra os comandos disponíveis.",
+    options: [],
+    run: async (client, interaction) => {
+    
+    interaction.reply("Olá, nesse momento tenho apenas 4 comandos.\n\n/ping mostra o meu tempo de resposta e quanto tempo estou online.\n\n/ready comando teste para saber se os comandos de '/' estão funcionando perfeitamente\n\n/simi O comando Simi permite que você interaja com o SimSimi, um chatbot inteligente capaz de responder às suas mensagens com muita zoeira e diversão\n\n/criador mostra quem me criou.")
+      
+    },
+    };
+    
